d10b195c0e4cb0b9f23d6fd7fce1b3d99601cbd4
Remy <remy.bleuer@hotmail.com>
2023-09-30 16:22:18 +0200

    Implemented read and write on files with command line

--- src/main/java/ch/heigvd/Commands.java
+++ src/main/java/ch/heigvd/Commands.java
@@ -1,21 +1,37 @@
 package ch.heigvd;
 
 import picocli.CommandLine;
+
 import java.io.*;
 import java.nio.charset.Charset;
 
-@CommandLine.Command(name="Cli", description="My custom CLI")
-public class Cli {
+@CommandLine.Command(name="commands", description="My custom CLI")
+class Commands implements Runnable {
 
     @CommandLine.Option(names = {"-h", "--help"}, description = "Information about the CLI")
     private String optHelp;
 
     @CommandLine.Option(names = {"-f", "--file"}, description = "Input file")
-    private String optInputFile;
+    public static String optInputFile;
 
     @CommandLine.Option(names = {"-o", "--output"}, description = "Output file")
-    private String optOutputFile;
+    public static String optOutputFile;
 
     @CommandLine.Option(names = {"-e", "--encoding"}, description = "File encoding")
     private Charset optEncoding;
+
+    public void run() {
+        String input = null;
+        try {
+            input = ReadWriteFiles.readFile(Commands.optInputFile);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        String output = input.toUpperCase();
+        try {
+            ReadWriteFiles.writeFile(output, Commands.optOutputFile);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
